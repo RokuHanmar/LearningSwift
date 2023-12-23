c24f49df1f749451e8baf7a467fe965b1c9f6c9c
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-12-23 16:28:50 +0000

    Create Ternary Conditional Operators.swift

---
+++ Ternary Conditional Operators.swift
@@ -0,0 +1,11 @@
+// An if/else statement on one line
+
+var cloudy = true
+
+if cloudy {
+  print("It's cloudy")
+} else {
+  print(It's not cloudy)
+       }
+
+cloudy ? print("It's cloudy") : print("It's not cloudy")
