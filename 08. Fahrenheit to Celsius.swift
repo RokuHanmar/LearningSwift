0c8e9dc568214b168568ed2b9e260daffc578428
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-12-23 15:34:49 +0000

    Create 08. Fahrenheit to Celsius.swift

---
+++ 08. Fahrenheit to Celsius.swift
@@ -0,0 +1,8 @@
+// Converts and outputs a given temperature in fahrenheit to Celsius
+// Uses variables, type annotations, arithmetic, string interpolation and print statements
+
+var tempf: Double = 36
+
+var tempc: Double = (tempf - 32) / 1.8
+
+print("The temperature is \(tempc) degrees Celsius")
