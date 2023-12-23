c9c4c09eb4e0600b4dbbb76d388158053c11347e
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-12-23 16:14:11 +0000

    Create 12. Comparison.swift

---
+++ 12. Comparison.swift
@@ -0,0 +1,9 @@
+// Compares the points of two teams and outputs a message depending on which is winning
+
+var bluePoints = 5
+var redPoints = 6
+
+if (bluePoints > redPoints) {
+  print("Blue team wins!")
+} else {
+  print("Red team wins!")
