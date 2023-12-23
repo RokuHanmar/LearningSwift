f3db8254f316326441b3ceeec4250a057002da45
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-12-23 15:48:44 +0000

    Create 09. BMI.swift

---
+++ 09. BMI.swift
@@ -0,0 +1,8 @@
+// Calculates BMI from the given values of weight and height
+// Uses variables, string interpolation, type annotations and arithmetic
+
+var weight: Double = 70
+var height: Double = 1.8
+
+var BMI = weight / (height * height)
+print("Your BMI is \(BMI))"
