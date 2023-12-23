d6ccd20fcac156a09fb0f254b0d6675f6766f7a5
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2023-12-23 16:21:50 +0000

    Create 13. Else If.swift

---
+++ 13. Else If.swift
@@ -0,0 +1,14 @@
+// Compares a given language to a number if possibilities, outputting the language if found and outputting "Language not found" if not
+
+var language = "French"
+
+if language == "French" {
+  print("Language is French")
+} else if language == "German" {
+  print("Language is German")
+} else if language == "Spanish" {
+  print("Language is Spanish")
+} else if language == "English" {
+  print("Language is English")
+} else {
+  print("Language not found")
